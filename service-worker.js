6152e0ad02bcb708cf2d3d37e38a1b7a8ed27492
Aditya Saurabh Singh <153707277+Saurabh-adi@users.noreply.github.com>
2024-01-24 23:48:28 +0530

    service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,12 @@
+//chrome.action api
+chrome.action.onClicked.addListener((tab) => {
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    func: () => {
+      alert("hello this is cricktension");
+    },
+  });
+});
+
+
+
